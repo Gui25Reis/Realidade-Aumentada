fd92234fee9674e911984f08ac2ab6b45c25349d
Gabriele Namie <gabrielenamie@hotmail.com>
2022-06-02 17:42:58 -0300

    Mudanças MainView
    - add constraints
    -criação botao start

--- Nano05-AR/Nano05-AR/View/MainView.swift
+++ Nano05-AR/Nano05-AR/View/MainView.swift
@@ -21,6 +21,9 @@ class MainView: UIView {
     /// Botão de tutorial
     private let helpButton = CustomViews.newButton()
     
+    /// Botão de começar
+    private let startButton = CustomViews.newButton()
+    
     
     /// Contador de fotos tiradas
     private let photosLabel = CustomViews.newLabel()
@@ -41,15 +44,22 @@ class MainView: UIView {
         
         self.addSubview(self.mainView)
         
-        self.addSubview(self.photosLabel)
         self.addSubview(self.helpButton)
+        self.addSubview(self.startButton)
         
+        self.addSubview(self.photosLabel)
         self.addSubview(self.timerLabel)
         
         for _ in 0..<3 {
             let lbl = CustomViews.newLabel()
+            lbl.text = Emojis.beijinho.description
+            lbl.font = .systemFont(ofSize: 65, weight: .black)
+            self.emojisLabels.append(lbl)
             self.addSubview(lbl)
         }
+        
+        let configIcon = UIImage.SymbolConfiguration(pointSize: 25, weight: .semibold, scale: .large)
+        self.helpButton.setImage(UIImage(systemName: "info.circle", withConfiguration: configIcon), for: .normal)
     }
     
     required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
@@ -88,7 +98,13 @@ class MainView: UIView {
         super.layoutSubviews()
         
         let space: CGFloat = 25
-        let square: CGFloat = 60
+        let square: CGFloat = 70
+        let safeArea: CGFloat = 50
+        self.photosLabel.backgroundColor = .secondaryLabel
+        self.photosLabel.text = "2/3"
+        self.photosLabel.font = .systemFont(ofSize: 20, weight: .bold)
+        self.timerLabel.text = "1"
+        self.timerLabel.font = .systemFont(ofSize: 90, weight: .black)
                 
         NSLayoutConstraint.activate([
             
@@ -96,7 +112,47 @@ class MainView: UIView {
             self.mainView.topAnchor.constraint(equalTo: self.topAnchor),
             self.mainView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
             self.mainView.leftAnchor.constraint(equalTo: self.leftAnchor),
-            self.mainView.rightAnchor.constraint(equalTo: self.rightAnchor)
+            self.mainView.rightAnchor.constraint(equalTo: self.rightAnchor),
+            
+            // Photos Label
+            self.photosLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: safeArea),
+            self.photosLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: space),
+            self.photosLabel.heightAnchor.constraint(equalToConstant: 40),
+            self.photosLabel.widthAnchor.constraint(equalToConstant: 60),
+            
+            // Help Button
+            self.helpButton.topAnchor.constraint(equalTo: self.topAnchor, constant: safeArea),
+            self.helpButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -space),
+            self.helpButton.heightAnchor.constraint(equalToConstant: 50),
+            self.helpButton.widthAnchor.constraint(equalToConstant: 50),
+            
+            self.startButton.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            self.startButton.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -safeArea),
+            self.startButton.heightAnchor.constraint(equalToConstant: square),
+            self.startButton.widthAnchor.constraint(equalToConstant: square),
+            
+            self.timerLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            self.timerLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            self.timerLabel.heightAnchor.constraint(equalToConstant: 120),
+            self.timerLabel.widthAnchor.constraint(equalToConstant: 80),
+            
+            self.emojisLabels[1].centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            self.emojisLabels[1].bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -safeArea),
+            self.emojisLabels[1].heightAnchor.constraint(equalToConstant: square),
+            self.emojisLabels[1].widthAnchor.constraint(equalToConstant: square),
+            
+            self.emojisLabels[0].centerYAnchor.constraint(equalTo: self.emojisLabels[1].centerYAnchor),
+            self.emojisLabels[0].bottomAnchor.constraint(equalTo: self.emojisLabels[1].bottomAnchor),
+            self.emojisLabels[0].heightAnchor.constraint(equalToConstant: square),
+            self.emojisLabels[0].widthAnchor.constraint(equalToConstant: square),
+            self.emojisLabels[0].rightAnchor.constraint(equalTo: self.emojisLabels[1].leftAnchor, constant: -space),
+            
+            self.emojisLabels[2].centerYAnchor.constraint(equalTo: self.emojisLabels[1].centerYAnchor),
+            self.emojisLabels[2].bottomAnchor.constraint(equalTo: self.emojisLabels[1].bottomAnchor),
+            self.emojisLabels[2].heightAnchor.constraint(equalToConstant: square),
+            self.emojisLabels[2].widthAnchor.constraint(equalToConstant: square),
+            self.emojisLabels[2].leftAnchor.constraint(equalTo: self.emojisLabels[1].rightAnchor, constant: space),
+
         ])
     }
 }
